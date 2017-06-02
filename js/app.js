c64f5b0d8eaa316ac0136c065e52e822c3808eee
MarionFoto <marionacp@gmail.com>
2017-06-01 22:05:46 -0400

    js funciona un poco

--- js/app.js
+++ js/app.js
@@ -24,3 +24,29 @@
 	});
 })();
 
+
+(function mainDos(){
+	var boxes = Array.from(document.getElementsByClassName("col-4"));
+	var modal = document.getElementById("conta");
+	var bodyModal, close, img;
+	boxes.forEach(function(box){
+		box.addEventListener("click", function(){
+			modal.innerHTML = "";
+			bodyModal = document.createElement("div");
+			bodyModal.classList.add("modal-body");
+			bodyModal.innerHTML = box.innerHTML;			
+			modal.appendChild(bodyModal);
+			modal.classList.remove("aparece");
+			close = document.createElement("div");
+			close.classList.add("close");
+			img = document.createElement("img");
+			img.setAttribute("src", "http://tojaeurope.com/gallery/Close-icon.png");
+			close.appendChild(img);
+			modal.appendChild(close);
+			close.addEventListener("click",function(){
+				modal.classList.add("conta");
+			});
+		});		
+	});
+})();
+
